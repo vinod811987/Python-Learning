50bdc661e8fece445599bcaf0178b9d1b855dcff
Vinod <vinodchoudhary87@gmail.com>
2026-05-26 16:36:57 +0530

    third program

---
+++ ThirdProgram.py
@@ -0,0 +1,33 @@
+# light = "orange"
+
+# if (light == "Red"):
+#     print("Stop")
+
+# elif (light == "orange"):
+#     print("look")
+
+# else:
+#     print("green")
+
+
+# marks = 80
+
+# if marks >= 90:
+#     print("Grade A")
+
+# elif marks >= 70:
+#     print("Grade B")
+
+# else:
+#     print("Grade C")
+
+marks = 40
+
+if marks >= 90:
+    print("Grade A")
+
+elif marks >= 70:
+    print("Grade B")
+
+else:
+    print("Grade C")
